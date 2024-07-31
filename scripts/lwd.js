6939ac8bceb58b80f36b1e0e4e1fe4b486f162a4
lwdwhen <lwdwhen@gmail.com>
2024-07-31 01:20:34 -0300

    create_lwd

--- scripts/lwd.js
+++ scripts/lwd.js
@@ -1,7 +1,7 @@
-console.log(await isAutorized());
-// createLwd();
+createLwd()
 
-function createLwd() {
+async function createLwd() {
+  if (!(await isAutorized())) return;
   document.body.outerHTML = "<body></body>";
   allTags = JSON.parse(localStorage.getItem("allTags")) || [];
 }
