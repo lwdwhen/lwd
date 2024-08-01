c7390422abb1a6ab7a3eb190205c4297641873e4
lwdwhen <lwdwhen@gmail.com>
2024-08-01 11:17:25 -0300

    auth_v2_and_mongo_js

--- scripts/lwd.js
+++ scripts/lwd.js
@@ -1,9 +1,25 @@
-createLwd()
+if (Mongo.ready()) createLwd();
+
+async function login() {
+  document.querySelector("[name=login]").setAttribute("disabled", true);
+  mongoUrl = document.querySelector("[name=mongoUrl]").value;
+  username = document.querySelector("[name=username]").value;
+  password = document.querySelector("[name=password]").value;
+  imgbbApiKey = document.querySelector("[name=imgbbApiKey]").value;
+
+  Mongo.projectId = mongoUrl.match(/\/data-[\w-]*/)[0].slice(1);
+  mongoRegionDotProvider = mongoUrl.match(/\/\/[\w-]*\.[\w]*/)[0].slice(2);
+  Mongo.provider = mongoRegionDotProvider.split(".")[1];
+  Mongo.region = mongoRegionDotProvider.split(".")[0];
+
+  Mongo.auth(username, password).then((data) => {
+    document.querySelector("[name=login]").removeAttribute("disabled");
+    if(data)location.reload();
+  });
+}
 
 async function createLwd() {
-  if (!(await isAutorized())) return;
   document.body.outerHTML = "<body></body>";
   allTags = JSON.parse(localStorage.getItem("allTags")) || [];
 }
 
-class test {}
---
+++ user-js-css/mongo.js
@@ -0,0 +1,166 @@
+class Mongo {
+  static accessToken = localStorage.getItem("mongoAccessToken");
+  static projectId = localStorage.getItem("mongoProjectId");
+  static provider = localStorage.getItem("mongoProvider");
+  static region = localStorage.getItem("mongoRegion");
+
+  static #authUrl = () =>
+    `https://${Mongo.region}.${Mongo.provider}.services.cloud.mongodb.com/api/client/v2.0/app/${Mongo.projectId}/auth/providers/local-userpass/login`;
+  static #ApiUrl = (action) =>
+    `https://${Mongo.region}.${Mongo.provider}.data.mongodb-api.com/app/${Mongo.projectId}/endpoint/data/v1/action/${action}`;
+  static #authHeaders = () => ({
+    "Access-Control-Request-Headers": "*",
+    Accept: "application/json",
+    Authorization: `Bearer ${Mongo.accessToken}`,
+    "Content-Type": "application/json",
+  });
+
+  static ready() {
+    return (
+      Mongo.accessToken && Mongo.projectId && Mongo.provider && Mongo.region
+    );
+  }
+
+  static async auth(username, password) {
+    return fetch(Mongo.#authUrl(), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        Mongo.accessToken = responseJson.access_token;
+        localStorage.setItem("mongoAccessToken", Mongo.accessToken)
+        localStorage.setItem("mongoProjectId", Mongo.projectId)
+        localStorage.setItem("mongoProvider", Mongo.provider)
+        localStorage.setItem("mongoRegion", Mongo.region)
+        return responseJson;
+      })
+      .catch((e) => false);
+  }
+
+  static async find(collection, data) {
+    if (data?.filter?._id) data.filter._id = { $oid: data.filter._id };
+
+    return fetch(apiUrl("find"), {
+      method: "POST",
+      headers: Mongo.#authHeaders(),
+      body: JSON.stringify({
+        dataSource: "lwd",
+        database: "lwd",
+        collection: collection,
+        limit: 50000,
+        ...data,
+      }),
+    }).then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        error = await response.json();
+        console.error(`Mongo.find: ${error}`);
+        throw error;
+      }
+    });
+  }
+
+  static async insert(collection, documents) {
+    createdAt = updatedAt = new Date().toJSON();
+    documents = documents.map((d) => ({ ...d, createdAt, updatedAt }));
+
+    return fetch(apiUrl("insertMany"), {
+      method: "POST",
+      headers: Mongo.#authHeaders(),
+      body: JSON.stringify({
+        dataSource: "lwd",
+        database: "lwd",
+        collection,
+        documents,
+      }),
+    }).then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        error = await response.json();
+        console.error(`Mongo.insert: ${error}`);
+        throw error;
+      }
+    });
+  }
+
+  static async insert(collection, documents) {
+    createdAt = updatedAt = new Date().toJSON();
+    documents = documents.map((d) => ({ ...d, createdAt, updatedAt }));
+
+    return fetch(apiUrl("insertMany"), {
+      method: "POST",
+      headers: Mongo.#authHeaders(),
+      body: JSON.stringify({
+        dataSource: "lwd",
+        database: "lwd",
+        collection,
+        documents,
+      }),
+    }).then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        error = await response.json();
+        console.error(`Mongo.insert: ${error}`);
+        throw error;
+      }
+    });
+  }
+
+  static async update(collection, filter, update) {
+    if (filter._id) filter._id = { $oid: filter._id };
+
+    updatedAt = new Date().toJSON();
+    if (!update.$set) update.$set = {};
+    update.$set = { ...update.$set, updatedAt };
+
+    return fetch(apiUrl("updateMany"), {
+      method: "POST",
+      headers: Mongo.#authHeaders(),
+      body: JSON.stringify({
+        dataSource: "lwd",
+        database: "lwd",
+        collection,
+        filter,
+        update,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.modifiedCount > 0) {
+          return responseJson;
+        } else {
+          console.warn(`Mongo.update: No matches found`);
+          throw `Mongo.update: No matches found`;
+        }
+      });
+  }
+
+  static async delete(collection, filter) {
+    if (filter._id) filter._id = { $oid: filter._id };
+
+    return fetch(apiUrl("deleteMany"), {
+      method: "POST",
+      headers: Mongo.#authHeaders(),
+      body: JSON.stringify({
+        dataSource: "lwd",
+        database: "lwd",
+        collection,
+        filter,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.deletedCount > 0) {
+          return responseJson;
+        } else {
+          console.warn(`Mongo.delete: No matches found`);
+          throw `Mongo.delete: No matches found`;
+        }
+      });
+  }
+}
