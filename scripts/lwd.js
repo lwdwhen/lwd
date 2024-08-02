36a944bed84a4593bd4e4a3b3973dddb185af974
lwdwhen <lwdwhen@gmail.com>
2024-08-02 00:00:22 -0300

    split_create_and_render_on_first_script

--- scripts/lwd.js
+++ scripts/lwd.js
@@ -1,4 +1,20 @@
 if (Mongo.ready()) createLwd();
+if (Mongo.ready()) window.onload = renderLwd;
+
+var allTags;
+const categoriesOrder = {
+  artist: 1,
+  copyright: 2,
+  character: 3,
+  species: 4,
+  general: 5,
+  unknown: 6,
+  lore: 7,
+  meta: 8,
+  invalid: 9,
+};
+const ratings = ["general", "sensitive", "questionable", "explicit"];
+
 
 async function login() {
   document.querySelector("[name=login]").setAttribute("disabled", true);
@@ -19,22 +35,6 @@ async function login() {
 }
 
 async function createLwd() {
-  document.body.outerHTML = "<body></body>";
-  allTags = JSON.parse(localStorage.getItem("allTags")) || [];
-
-  const categoriesOrder = {
-    artist: 1,
-    copyright: 2,
-    character: 3,
-    species: 4,
-    general: 5,
-    unknown: 6,
-    lore: 7,
-    meta: 8,
-    invalid: 9,
-  };
-  const ratings = ["general", "sensitive", "questionable", "explicit"];
-
   // lastSearch = undefined;
   // deleteMode = false;
   // sortBy = LwdHashRouter.get("sortBy");
@@ -49,7 +49,11 @@ async function createLwd() {
   //   textContent: tag.name,
   //   className: tag.category,
   // }));
+}
 
+async function renderLwd(){
+  document.body.outerHTML = "<body></body>";
+  
   document.body.append((sideSection = createSideSection()));
   document.body.append((topSection = createTopSection()));
 }
